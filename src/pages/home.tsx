53fe9dc0e97ab201609bf83588922120d6f1c1bc
abbos-t <tillahodjaev.abbos@gmail.com>
2024-04-29 05:54:46 +0100

    change get started link

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -53,7 +53,7 @@ function HomePage() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
-                to="/"
+                to="/login"
                 className="rounded-lg bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
